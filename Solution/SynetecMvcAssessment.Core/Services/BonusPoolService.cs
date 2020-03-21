fc6111b2f5f1e005cac97f933461ea52302bae07
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2020-03-21 12:12:24 +0000

    Added uncaught exception logging.
    Both Rolling file and Event logging enabled. Parameters are configurable from the app settings section of the web config. Note that event logging requires Admin perms so I disabled this by default in favour of rolling files but I left the option there.

--- Solution/SynetecMvcAssessment.Core/Services/BonusPoolService.cs
+++ Solution/SynetecMvcAssessment.Core/Services/BonusPoolService.cs
@@ -63,5 +63,7 @@ namespace SynetecMvcAssessment.Core.Services
             return result;
         }
 
+
+
     }
 }
--- Solution/SynetecMvcAssessment/Global.asax.cs
+++ Solution/SynetecMvcAssessment/Global.asax.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Serilog;
 
 namespace InterviewTestTemplatev2
 {
@@ -8,10 +12,31 @@ namespace InterviewTestTemplatev2
     {
         protected void Application_Start()
         {
+            //Note: Logs are written to a rolling log file that does not continually grow beyond a specified capacity. 
+            // I've gone with the default values though this could be configured.
+
+            var appSettings = ConfigurationManager.AppSettings;
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+                
+            Log.Error($"Synetec Mvc Assessment Started at UTC: { DateTime.Now.ToUniversalTime() } ");
+            Log.CloseAndFlush();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            //Catch Exception in Central place
+            var exception = Server.GetLastError();
+            
+            // We'll log errors that are caught here as fatal as they caused a 500 response.
+            Serilog.Log.Fatal(exception.Message);
+  
         }
     }
 }
